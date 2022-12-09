e3e758424a0823e778d1b0321a5aa12904b92210
Aditya Negara <aditnegarabisnis@gmail.com>
2022-12-09 13:31:43 +0700

    submission-1 animating navbar

--- submission-1/src/components/organisms/Navbar.jsx
+++ submission-1/src/components/organisms/Navbar.jsx
@@ -5,43 +5,68 @@ import searchIcon from "../../assets/icons/search.png";
 import createIcon from "../../assets/icons/plus.png";
 import dotsIcon from "../../assets/icons/dots.png";
 import { useTheme } from "@emotion/react";
+import { motion } from "framer-motion";
 
-const NavbarStyled = styled.nav({
+const NavbarStyled = styled(motion.nav)({
   marginTop: "3vh",
   display: "flex",
   justifyContent: "flex-end",
   gap: "15px",
 });
 
+const navbarVariant = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.3,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const buttonVariant = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: -100 },
+};
+
 const Navbar = () => {
   const theme = useTheme();
   return (
     <>
-      <NavbarStyled>
-        <Button
-          onClick={() => {}}
-          buttonColor={theme.colors.primary.normal}
-          buttonHoverColor={theme.colors.primary.darker}
-          role="search-button"
-        >
-          <img src={searchIcon} alt="search-icon" />
-        </Button>
-        <Button
-          onClick={() => {}}
-          buttonColor={theme.colors.primary.normal}
-          buttonHoverColor={theme.colors.primary.darker}
-          role="create-button"
-        >
-          <img src={createIcon} alt="create-icon" />
-        </Button>
-        <Button
-          onClick={() => {}}
-          buttonColor={theme.colors.primary.normal}
-          buttonHoverColor={theme.colors.primary.darker}
-          role="archived-tooltip_button"
-        >
-          <img src={dotsIcon} alt="dots-icon" />
-        </Button>
+      <NavbarStyled initial="hidden" animate="visible" variants={navbarVariant}>
+        <motion.div variants={buttonVariant}>
+          <Button
+            onClick={() => {}}
+            buttonColor={theme.colors.primary.normal}
+            buttonHoverColor={theme.colors.primary.darker}
+            role="search-button"
+          >
+            <img src={searchIcon} alt="search-icon" />
+          </Button>
+        </motion.div>
+        <motion.div variants={buttonVariant}>
+          <Button
+            onClick={() => {}}
+            buttonColor={theme.colors.primary.normal}
+            buttonHoverColor={theme.colors.primary.darker}
+            role="create-button"
+          >
+            <img src={createIcon} alt="create-icon" />
+          </Button>
+        </motion.div>
+        <motion.div variants={buttonVariant}>
+          <Button
+            onClick={() => {}}
+            buttonColor={theme.colors.primary.normal}
+            buttonHoverColor={theme.colors.primary.darker}
+            role="archived-tooltip_button"
+          >
+            <img src={dotsIcon} alt="dots-icon" />
+          </Button>
+        </motion.div>
       </NavbarStyled>
     </>
   );
