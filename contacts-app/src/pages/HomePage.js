05c6d15ee5e4e8ef77dc58b9ba3a7c83521e7665
Aditya Negara <aditnegarabisnis@gmail.com>
2022-12-22 10:18:39 +0700

    Finish CRUD contact

--- contacts-app/src/pages/HomePage.js
+++ contacts-app/src/pages/HomePage.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { useSearchParams } from 'react-router-dom';
-import ContactList from '../components/ContactList';
-import SearchBar from '../components/SearchBar';
-import { deleteContact, getContacts } from '../utils/data';
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import ContactList from "../components/ContactList";
+import SearchBar from "../components/SearchBar";
+import { getContacts, deleteContact } from "../utils/api";
 
 function HomePageWrapper() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const keyword = searchParams.get('keyword');
+  const keyword = searchParams.get("keyword");
 
   function changeSearchParams(keyword) {
     setSearchParams({ keyword });
   }
 
-  return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+  return (
+    <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+  );
 }
 
 class HomePage extends React.Component {
@@ -21,22 +23,30 @@ class HomePage extends React.Component {
     super(props);
 
     this.state = {
-      contacts: getContacts(),
-      keyword: props.defaultKeyword || '',
-    }
+      contacts: [],
+      keyword: props.defaultKeyword || "",
+    };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
     this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
   }
 
-  onDeleteHandler(id) {
-    deleteContact(id);
-
-    // update the contact state from data.js
+  async componentDidMount() {
+    const { data } = await getContacts();
     this.setState(() => {
       return {
-        contacts: getContacts(),
-      }
+        contacts: data,
+      };
+    });
+  }
+
+  async onDeleteHandler(id) {
+    await deleteContact(id);
+    const { data } = await getContacts();
+    this.setState(() => {
+      return {
+        contacts: data,
+      };
     });
   }
 
@@ -44,7 +54,7 @@ class HomePage extends React.Component {
     this.setState(() => {
       return {
         keyword,
-      }
+      };
     });
 
     this.props.keywordChange(keyword);
@@ -52,18 +62,21 @@ class HomePage extends React.Component {
 
   render() {
     const contacts = this.state.contacts.filter((contact) => {
-      return contact.name.toLowerCase().includes(
-        this.state.keyword.toLowerCase()
-      );
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.keyword.toLowerCase());
     });
 
     return (
       <section>
         <h2>Daftar Kontak</h2>
-        <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler} />
+        <SearchBar
+          keyword={this.state.keyword}
+          keywordChange={this.onKeywordChangeHandler}
+        />
         <ContactList contacts={contacts} onDelete={this.onDeleteHandler} />
       </section>
-    )
+    );
   }
 }
 
