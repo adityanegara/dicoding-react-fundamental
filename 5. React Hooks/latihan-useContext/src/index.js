08e663c98bb093da5ed7f3f6fd552177e4a0de1b
Aditya Negara <aditnegarabisnis@gmail.com>
2022-12-27 14:31:50 +0700

    Finish practice useContext

---
+++ 5. React Hooks/latihan-useContext/src/index.js
@@ -0,0 +1,59 @@
+import React, { useState, useMemo } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Community from './pages/Community';
+import Home from './pages/Home';
+import Tutorial from './pages/Tutorial';
+import LocaleContext from './contexts/LocaleContext';
+
+import './styles/style.css';
+
+/**
+ * @todos
+ * 1. Buatlah fitur ubah bahasa dengan memanfaatkan Context.
+ * 2. Pastikan Anda menggunakan fitur Hooks dalam memanfaatkan Context.
+ *
+ * Catatan:
+ *  - Manfaatkan tombol yang berada di pojok kanan navigasi untuk mengubah bahasa.
+ *  - Seluruh konten yang ditampilkan diambil dari utils -> content.js
+ */
+
+function App() {
+  const [locale, setLocale] = useState('id');
+
+  const toggleLocale = () => {
+    setLocale((prevLocale) => {
+      return prevLocale === 'id' ? 'en' : 'id';
+    });
+  };
+
+  const contextValue = useMemo(() => {
+    return {
+      locale,
+      toggleLocale
+    };
+  }, [locale]);
+
+  return (
+    <LocaleContext.Provider value={contextValue}>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+          <Route path="/community" element={<Community />} />
+        </Routes>
+      </main>
+    </LocaleContext.Provider>
+  );
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
---
+++ 5. React Hooks/latihan-useContext/src/pages/Tutorial.js
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+import Posts from '../components/Posts';
+import LocaleContext from '../contexts/LocaleContext';
+
+function Tutorial() {
+  const { locale } = useContext(LocaleContext);
+  return (
+    <>
+      <h1>Tutorial</h1>
+      <Posts locale={locale} />
+    </>
+  );
+}
+
+export default Tutorial;
